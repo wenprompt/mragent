d89939ad73535772527030a422bc39ef650c04ba
wenprompt <wenprompt@gmail.com>
2025-06-28 20:30:35 +0800

    07-agent-tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,7 +1,26 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandBox(sandboxId: string) {
   //establish a live connection to an existing sandbox instance
   const sandbox = await Sandbox.connect(sandboxId);
   return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  // Find the last message from the assistant in the output and if it containts the <Task Summary> which we defined in the PROMPT
+  // result have a .output property because type is AgentResult
+  const lastAssistantTextMessageIndex = result.output.findLastIndex(
+    (message) => message.role === "assistant"
+  );
+
+  const message = result.output[lastAssistantTextMessageIndex] as
+    | TextMessage
+    | undefined;
+
+  return message?.content
+    ? typeof message.content === "string"
+      ? message.content
+      : message.content.map((c) => c.text).join("")
+    : undefined;
 }
